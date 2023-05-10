6b3b7522ec3a6160739a92ecfc10e412f01aafc1
Ahmad Dzulfikar Ubaidillah <dzulfprojects@gmail.com>
2023-05-10 16:53:10 +0700

    add static files

--- auth/settings.py
+++ auth/settings.py
@@ -121,21 +121,12 @@ USE_I18N = True
 
 USE_TZ = True
 
-import os
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-("js", os.path.join(STATIC_ROOT, 'js')),
-("css", os.path.join(STATIC_ROOT, 'css')),
-("images", os.path.join(STATIC_ROOT, 'images')),
-("fonts", os.path.join(STATIC_ROOT, 'fonts')),
-)
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "static"
 
 
 # Default primary key field type
